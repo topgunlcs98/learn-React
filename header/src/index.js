d3429648778747f4628f022ab909a8378bf56783
liuchangshu <liuchasngshu98@163.com>
2018-03-28 08:30:31 +0800

    'p'

--- header/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Container from './components/container/container';
-import registerServiceWorker from './registerServiceWorker';
-
-
-ReactDOM.render(<Container/>, document.getElementById('root'));
-registerServiceWorker();
